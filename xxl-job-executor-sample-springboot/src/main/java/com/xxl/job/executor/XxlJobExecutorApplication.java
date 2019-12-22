c6d1b1ce59cb7c702bfb66f1fbefa92642375aec
guos <962131438@qq.com>
2019-12-22 20:06:21 +0800

    init

--- xxl-job-executor-sample-springboot/src/main/java/com/xxl/job/executor/XxlJobExecutorApplication.java
+++ xxl-job-executor-sample-springboot/src/main/java/com/xxl/job/executor/XxlJobExecutorApplication.java
@@ -2,11 +2,13 @@ package com.xxl.job.executor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author xuxueli 2018-10-28 00:38:13
  */
 @SpringBootApplication
+@ComponentScan({"com.xxl"})
 public class XxlJobExecutorApplication {
 
 	public static void main(String[] args) {
--- xxl-job-executor-sample-springboot/src/test/java/com/xxl/job/executor/test/XxlJobExecutorExampleBootApplicationTests.java
+++ xxl-job-executor-sample-springboot/src/test/java/com/xxl/job/executor/test/XxlJobExecutorExampleBootApplicationTests.java
@@ -1,7 +1,7 @@
 package com.xxl.job.executor.test;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.runner.RunWith;;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
